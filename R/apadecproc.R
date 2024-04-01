9c5c9a1d55b3c57853b2e94dba933302f051707a
fawda123 <mbafs2012@gmail.com>
2024-04-01 08:37:48 -0400

    apadecproc by year

--- R/apadecproc.R
+++ R/apadecproc.R
@@ -23,16 +23,30 @@ apadat <- apadatraw |>
 tomod <- apadat |> 
   select(-DO_dtd)
 
-# setup parallel backend
-ncores <- detectCores()
-cl <- makeCluster(ncores - 2)
-registerDoParallel(cl)
+yrs <- unique(year(tomod$DateTimeStamp))
 
-# ebase
-apadecobs <- ebase(tomod, interval = 900, Z = tomod$Depth, ndays = 7, progress = getwd(), n.chains = 4,
+apadecobs <- NULL
+for(yr in yrs){
+  
+  cat(yr, '\t')
+  
+  # setup parallel backend
+  ncores <- detectCores()
+  cl <- makeCluster(ncores - 2)
+  registerDoParallel(cl)
+
+  tomodsub <- tomod |> 
+    filter(year(DateTimeStamp) == yr)
+  
+  # ebase
+  res <- ebase(tomodsub, interval = 900, Z = tomodsub$Depth, ndays = 7, progress = getwd(), n.chains = 4,
              bprior = c(0.251, 1e-6))
 
-stopCluster(cl)
+  stopCluster(cl)
+  
+  apadecobs <- rbind(apadecobs, res)
+
+}
 
 save(apadecobs, file = here('data/apadecobs.RData'))
 
@@ -42,15 +56,29 @@ tomod <- apadat |>
   select(-DO_obs) |> 
   rename(DO_obs = DO_dtd)
 
-# setup parallel backend
-ncores <- detectCores()
-cl <- makeCluster(ncores - 2)
-registerDoParallel(cl)
+yrs <- unique(year(tomod$DateTimeStamp))
 
-# ebase
-apadecdtd <- ebase(tomod, interval = 900, Z = tomod$Depth, ndays = 7, progress = getwd(), n.chains = 4,
-                bprior = c(0.251, 1e-6))
+apadecdtd <- NULL
+for(yr in yrs){
+  
+  cat(yr, '\t')
+  
+  # setup parallel backend
+  ncores <- detectCores()
+  cl <- makeCluster(ncores - 2)
+  registerDoParallel(cl)
 
-stopCluster(cl)
+  tomodsub <- tomod |> 
+    filter(year(DateTimeStamp) == yr)
+  
+  # ebase
+  res <- ebase(tomodsub, interval = 900, Z = tomodsub$Depth, ndays = 7, progress = getwd(), n.chains = 4,
+                  bprior = c(0.251, 1e-6))
+
+  stopCluster(cl)
+  
+  apadecdtd <- rbind(apadecdtd, res)
+  
+}
 
 save(apadecdtd, file = here('data/apadecdtd.RData'))
