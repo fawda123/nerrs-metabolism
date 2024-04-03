1a4701a94b67ea4eb9d049664062b5baaf439f38
fawda123 <mbafs2012@gmail.com>
2024-04-03 07:32:37 -0400

    save apadecdtd as single file for all years, add plot code to apadecproc.R

--- R/apadecproc.R
+++ R/apadecproc.R
@@ -1,9 +1,9 @@
-library(dplyr)
-library(lubridate)
+library(tidyverse)
 library(EBASE)
 library(doParallel)
 library(foreach)
 library(here)
+library(plotly)
 
 # prep apa data
 apadatraw <- read.csv(here('data-raw/cp_ebase_911.csv'))
@@ -83,3 +83,41 @@ for(yr in yrs){
 }
 
 save(apadecdtd, file = here('data/apadecdtd.RData'))
+
+# view results --------------------------------------------------------------------------------
+
+data(apadecobs)
+data(apadecdtd)
+
+ylab <- 'mmol O2 m-2 d-1)'
+
+apadec <- list(
+  Observed = apadecobs, 
+  Detided = apadecdtd
+) |> 
+  enframe(name = 'Type') |> 
+  unnest(value) |> 
+  select(Type, Date, P, R, D) |> 
+  mutate(
+    NEM = P - R
+  ) |> 
+  pivot_longer(cols = -matches('Date|Type')) |> 
+  summarise(value = mean(value, na.rm = T), .by = c(Type, Date, name)) |> 
+  pivot_wider(names_from = name, values_from = value)
+
+p1 <- plot_ly(subset(apadec, Type == 'Observed'), x = ~Date, y = ~P, type = 'scatter', mode = 'lines', name = 'P') |>
+  add_trace(y = ~-R, mode = 'lines', name = 'R') |>
+  add_trace(y = ~D, mode = 'lines', name = 'D') |>
+  add_trace(y = ~NEM, mode = 'lines', name = 'NEM') |>
+  layout(xaxis = list(title = ''),
+         yaxis = list(title = paste('Observed', ylab)))
+
+p2 <- plot_ly(subset(apadec, Type == 'Detided'), x = ~Date, y = ~P, type = 'scatter', mode = 'lines', name = 'P') |>
+  add_trace(y = ~-R, mode = 'lines', name = 'R') |>
+  add_trace(y = ~D, mode = 'lines', name = 'D') |>
+  add_trace(y = ~NEM, mode = 'lines', name = 'NEM') |>
+  layout(xaxis = list(title = ''),
+         yaxis = list(title = paste('Detided', ylab)))
+
+subplot(p1, p2, nrows = 2, shareX = T, titleY = T)
+
